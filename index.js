92fbe4c6d4eaa38f3bcd0bdd37c53ca45bf57c2a
David Espinoza <davidespinoza@att.net>
2018-01-10 23:30:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function distanceTravelledInFeet(beg, end){
 function calculatesFarePrice(start, destination){
   if((destination - start) >= 400 && (destination - start) <= 2000){
     return (destination - start) * .02;
+  }else{
+    return "yo";
   }
 }
