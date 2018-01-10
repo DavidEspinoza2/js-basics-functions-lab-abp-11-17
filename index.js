34d320e66301503f421b232ae9aba39837497571
David Espinoza <davidespinoza@att.net>
2018-01-10 22:52:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+  return Math.abs(42 - pickupLocation);
+}
+
+function calculateVertical(beg, end){
+  return (end - beg) * 264;
+}
+
+function distanceFromHqInFeet(){
+  return
+}
